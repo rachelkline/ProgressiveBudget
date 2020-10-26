1233a9eee6159ceb024f7c0aa2fe8f7ff4aadb0c
Rachel Kline <rach3lkline@gmail.com>
2020-10-26 16:28:20 -0400

    Fixed typo

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(provess.env.MOGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MOGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
